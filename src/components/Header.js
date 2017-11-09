458b1ad91bff78cfe4df9b31fcaf71a0f31b6238
Josh Kennedy <jshknndy@gmail.com>
2017-11-09 13:49:59 -0600

    MOAR SETUP

---
+++ src/components/Header.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+class Header extends React.Component {
+  constructor() {
+    super();
+  }
+
+  render() {
+    return (
+      <header>
+        <nav>
+          <a href="#">Home</a>
+        </nav>
+      </header>
+    )
+  }
+}
+
+export default Header;
---
+++ src/components/Layout.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class Layout extends React.Component {
+  constructor() {
+    super();
+  }
+
+  render() {
+    return (
+      <section className="Layout">
+
+      </section>
+    )
+  }
+}
+
+export default Layout;
